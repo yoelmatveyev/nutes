058d65b02d13546e397a1c236aacd754ec381c94
yoelmatveyev <yoelmatveyev@gmail.com>
2019-10-30 18:50:03 +0300

    Update nutes.lisp

--- nutes.lisp
+++ nutes.lisp
@@ -1,48 +1,42 @@
+(load "lisp/nutes/ternary-print.lisp")
+(load "lisp/nutes/input-output.lisp")
+(load "lisp/nutes/assembler.lisp")
+(load "lisp/nutes/instructions.lisp")
+(load "lisp/nutes/examples.lisp")
+
 (defstruct tape
-  width
   position
   initial
   vector
   length
   halted
   special
+  counter
   )
 
 ;; The 'special' field in tapes is reserved for IO and other later extensions
 
-(defmacro initiate-tape
-    (name length width &optional (initial 0) (special 27))
-      `(setf ,name
-       	(make-tape :width ,width :position ,initial :initial ,initial
-		   :vector ,(make-array length) :length ,length
-		   :halted nil :special ,special)))
+(defun create-tape
+    (length &optional (position 0) (counter 0))
+		   (make-tape
+		    :position position
+		    :initial 0
+		    :vector (make-array length :element-type '(signed-byte 64))
+		    :length length
+		    :halted nil
+		    :special nil
+		    :counter counter))
 
 ;; We convert integers to balanced ternary numbers of a given range
 
-(defun coerce-width (x &optional (width 9))
-  (let* ((power (expt 3 width))
-	 (range (/ (1- power) 2))
-	 (n (mod x power)))
+(defvar width 36)
+(defvar power (expt 3 width))
+(defvar range (/ (1- power) 2))
+
+(defun coerce-width (x)
+  (let ((n (mod x power)))
     (if (> n range)
 	(- n power) n)))
-
-;; We feed a list of commands to a tape
-
-(defun program-tape (name list &optional (offset 0))
-  (loop for p from 0 to (1- (length list))
-      do (setf (elt (tape-vector name)
-		    (mod (+ p offset) (tape-length name)))
-	       (coerce-width (pop list) (tape-width name)))))
-
-;; Generator of modulo sum functions
-
-(defun mod+ (m)
-  (lambda (&rest x) (mod (apply #'+ x) m)))
-
-;; Ternary analog of XOR (might be useful for something)
-
-(defun ternary-xor (x y)
-  (1- (mod (+ 1 (signum x)(signum y))3)))
 
 ;; Sign sum function
 
@@ -52,46 +46,67 @@
 ;; Our main 'CPU'
 
 (defun one-step (tape)
+  (declare (optimize speed (safety 0)(debug 0)))
   (let*
       ((p (tape-position tape))
        (v (tape-vector tape))
        (l (tape-length tape))
-       (p+ (elt v (mod (- p 1) l)))
-       (p- (elt v (mod (+ p 1) l)))
+       (p+ (elt v (mod (1- p ) l)))
+       (p- (elt v (mod (1+ p ) l)))
        (j (elt v (mod p l)))
-       (j- (elt v (mod (+ p j -1) l)))
-       (j+ (elt v (mod (+ p j 1) l)))
-       (j0 (elt v (mod (+ p j) l)))
+       (pj (+ p j))
+       (j- (elt v (mod (1- pj ) l)))
+       (j+ (elt v (mod (1+ pj ) l)))
+       (j0 (elt v (mod pj l)))
        (a+ (elt v (mod (+ p p+) l)))
        (a- (elt v (mod (+ p p-) l)))
-       (sub (coerce-width (- a+ a-) (tape-width tape)))
+       (sub (coerce-width (- a+ a-)))
        (sign (sign-sum a+ a-)))
+    (declare (type fixnum p))
+    (declare (type fixnum l))
+    (declare (type fixnum p+))
+    (declare (type fixnum p-))
+    (declare (type fixnum j))
+    (declare (type fixnum j-))
+    (declare (type fixnum j+))
+    (declare (type fixnum j0))
+    (declare (type fixnum a+))
+    (declare (type fixnum a-))
+    (declare (type fixnum sub))
+    (declare (type fixnum sign))
+    (incf (tape-counter tape))
     (setf (elt v (mod (+ p p+) l)) sub)
     (setf (elt v (mod (+ p p-) l)) (- sub))
     (case sign
       (1 (setf (tape-position tape) (mod (+ p j+) l)))
       (0 (setf (tape-position tape) (mod (+ p j0) l)))
       (-1 (setf (tape-position tape) (mod (+ p j-) l))))
-	(if (and (zerop j0) (zerop sub))
-	    (progn (setf (tape-halted tape) t)
-	    (list (elt v (mod (- p 1) l))
-	    (elt v (mod (+ p 1) l)))
-	    (print "Program halted")))))
+    (if (and (zerop j0) (zerop sub))
+	(progn (setf (tape-halted tape) t)
+	       (setf (tape-special tape) a+))
+	t)))
 
+;; Write your program using the tools provides by the file insructions.lisp and feed it to the tape
+
+(defun load-tape (tape prg &optional (offset 0))
+  (let ((list))
+  (if
+   (not (listp prg))
+	(progn
+	  (setf list (append (program-data prg) (program-code prg)))
+	  (setf offset (program-first prg)))
+	(setf list prg))
+  (loop for p from 0 to (1- (length list))
+     do (setf (elt (tape-vector tape)
+		   (mod (+ p offset) (tape-length tape)))
+              (coerce-width (pop list))))))
 
 ;; Running our tape
 
-(defun run-tape (tape &optional (steps (expt 3 15)))
-(one-step tape)
+(defun run-tape (tape)
+  (loop do
+     (one-step tape)
+     while (not (tape-halted tape)))
   (if (tape-halted tape)
-      (progn
-	    (terpri)(print "Tape halted"))
-      (if (> steps 0) (run-tape tape (1- steps))
-      (progn
-	(terpri)
-	(print "Hm.. no termination after so many steps? Giving up for now")))))
-	  
+	(format t "~&Tape halted~&")))
 
-
-
-  
