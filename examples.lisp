eb3f94a058c13c2bbf19832619a6e188a9d1e241
yoelmatveyev <yoelmatveyev@gmail.com>
2019-11-08 10:27:32 +0300

    Add files via upload

--- examples.lisp
+++ examples.lisp
@@ -1,6 +1,14 @@
-;; Some practical examples to add to your program
+;;; Some practical examples to add to your program
 
-; Slow multiplication by repeating addition, a must be positive
+;; Example of usage:
+
+; (defparameter prg1 (create-prg))
+; (hello prg1)
+; (defparameter tape1 (create-tape (expt 3 6)))(load-tape tape1 prg1)
+; (run-tape tape1)				       
+
+
+; Slow multiplication by repeating addition, A must be positive
 ; b=a*b
 
  (defun add-smul (prg a b &key label (jmp (jmp+3 prg)))
@@ -26,7 +34,7 @@
     (add-one-inst prg 'z3 'z3 :label next :jmp jmp)))
 
 ;; Factorial
-
+  
 (defun add-factorial (prg n res &key label (jmp (jmp+3 prg)))
   (let ((loop (gensym))(end (gensym))(next (gensym)))
   (add-var prg 'v=1 1)
@@ -35,3 +43,18 @@
   (add-jsub prg 'v=1 n :label loop :jmp (list end end next))
   (add-smul prg n res :label next :jmp loop)
   (add-jmp prg 'z1 :label end :jmp jmp)))
+
+;; Basic input (up to 6 chars) and "Hello World"
+
+(defun hello (prg)
+  (add-var prg 'j1 8)
+  (add-var prg 'hello (char-36 "Hello"))
+  (add-var prg 'world (char-36 "World,"))
+  (add-var prg 'name)
+  (add-var prg 'input (char-36 "Name?"))
+  (add-io prg 'j1 'input 'input (op-gen (list (+ (expt 3 8)(expt 3 7)) 1 0 0)))
+  (add-io prg 'j1 'name 'name (op-gen (list (+ (expt 3 8) (expt 3 7)) 2 0 0)))
+  (add-io prg 'j1 'hello 'world (op-gen (list (expt 3 8) 1 0 0)))
+  (add-io prg 'j1 'name 'name (op-gen (list (+ (expt 3 8)(expt 3 7)) 1 0 0))))
+			
+					
